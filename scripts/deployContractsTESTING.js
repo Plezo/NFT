8cfe53f9c49fcdc5597e6b799cee866570333d67
Patryk Lezon <Plezo@protonmail.com>
2022-03-19 19:27:18 -0400

    Made deploy script for testing

--- scripts/deployContractsTESTING.js
+++ scripts/deployContractsTESTING.js
@@ -23,9 +23,22 @@ async function main() {
     await land.deployed();
     console.log("Land deployed to:", land.address);
 
+    // Enable sale
+    await warrior.connect(owner).flipSaleState();
+
     // Set contract addresses to land, and edit GM's for $RESOURCE
     await warrior.connect(owner).setContractAddresses(land.address, resource.address);
     await resource.connect(owner).editGameMasters([warrior.address, land.address], [true, true]);
+
+    // Set land claim to 0 seconds for testing purposes
+    await warrior.connect(owner).setLandClaimTime(0);
+    
+    // Set game parameters for testing purposes
+    await land.connect(owner).setVars(ethers.utils.parseEther("10"), ethers.utils.parseEther("1000000"), 120, 10, 1);
+
+    // need to mint token id 0 due to issues with code (idk if I need to "fix")
+    await warrior.connect(owner).ownerMint(false);
+    await land.connect(owner).mintLand(owner.address, 1);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
